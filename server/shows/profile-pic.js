73fff8bdf2e214b92127f60848407d8eb597ec30
Charles Lehner <celehner1@gmail.com>
2012-09-08 16:45:01 -0400

    Send empty images inline instead of redirect

--- server/shows/profile-pic.js
+++ server/shows/profile-pic.js
@@ -8,12 +8,9 @@ function(doc, req) {
 		};
 	} else {
 		return {
-			code: 301,
-			headers: {Location: "../profile-pic.gif"}
+			code: 404,
+			headers: {"Content-Type": "image/gif"},
+			base64: "R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
 		};
-		//headers: {"Content-Type": "image/gif"},
-		//body: "GIF89a\1\0\1\0\0\0\0\41\371\4\1\12\0\1\0\54\0\0\0\0\1\0\1\0\0\2\2\114\1\0;"
-		//headers: {Location: "data:image/gif,GIF89a%01%00%01%00%00%00%00%21%F9%04%01%0A%00%01%00%2C%00%00%00%00%01%00%01%00%00%02%02L%01%00%3B"}
-		//body: String.fromCharCode(71,73,70,56,57,97,1,0,1,0,0,0,0,33,249,4,1,10,0,1,0,44,0,0,0,0,1,0,1,0,0,2,2,76,1,0,59)
 	}
 }
